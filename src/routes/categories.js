1c305b8f30563eaae2242fa508e4966cc03884b8
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:29:01 +0500

    Update categories.js

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const { getCategories } = require('../controllers/productController');
+
+router.get('/', getCategories);
+
+module.exports = router;
 
