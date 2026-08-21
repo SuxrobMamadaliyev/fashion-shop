87d49deb61512f67e3995b30416e5cdf88e135dc
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:35:54 +0500

    Update validators.js

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,1 +1,10 @@
+function isValidPhone(phone) {
+  return typeof phone === 'string' && /^\+?[0-9]{9,15}$/.test(phone.replace(/[\s\-()]/g, ''));
+}
+
+function isNonEmptyString(val) {
+  return typeof val === 'string' && val.trim().length > 0;
+}
+
+module.exports = { isValidPhone, isNonEmptyString };
 
