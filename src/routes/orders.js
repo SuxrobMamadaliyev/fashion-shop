ad73e026fc4d86324afcdee58f6a3ef4fd3d2db7
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:30:12 +0500

    Update orders.js

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { requireTelegramAuth } = require('../middleware/telegramAuth');
+const { createOrder, getMyOrders, getOrderById } = require('../controllers/orderController');
+
+router.use(requireTelegramAuth);
+router.post('/', createOrder);
+router.get('/', getMyOrders);
+router.get('/:id', getOrderById);
+
+module.exports = router;
 
