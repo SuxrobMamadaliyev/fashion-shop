8acc228538a6954a939e9abdfcb9193daf71f25a
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:36:23 +0500

    Update catchAsync.js

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,1 +1,5 @@
+// Wraps async route handlers so thrown errors are forwarded to Express's error middleware
+module.exports = function catchAsync(fn) {
+  return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+};
 
