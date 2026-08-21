d68dd5260655f50e22ddbfefc8ea8daa251f0c2a
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:29:41 +0500

    Update favorites.js

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { requireTelegramAuth } = require('../middleware/telegramAuth');
+const { getFavorites, addFavorite, removeFavorite } = require('../controllers/favoriteController');
+
+router.use(requireTelegramAuth);
+router.get('/', getFavorites);
+router.post('/', addFavorite);
+router.delete('/:productId', removeFavorite);
+
+module.exports = router;
 
