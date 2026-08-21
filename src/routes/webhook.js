400ce06e827480375d8bc7361aaa8453d4855166
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:31:19 +0500

    Update webhook.js

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,1 +1,11 @@
 
+const express = require('express');
+const router = express.Router();
+const bot = require('../bot/bot');
+
+// Telegram will POST updates to this endpoint when USE_WEBHOOK=true.
+router.post('/', (req, res) => {
+  bot.handleUpdate(req.body, res);
+});
+
+module.exports = router;
