369a70a5c3e7e070a1baecf944288cabd6c60215
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:44:50 +0500

    Update mainMenu.js

--- src/bot/keyboards/mainMenu.js
+++ src/bot/keyboards/mainMenu.js
@@ -1,1 +1,20 @@
+const { Markup } = require('telegraf');
+
+function mainMenuKeyboard(webAppUrl) {
+  return Markup.keyboard([
+    [Markup.button.webApp('🛍 Do\'konni ochish', webAppUrl)],
+    ['📦 Katalog', '🆕 Yangi kolleksiya'],
+    ['🔥 Ko\'p sotilganlar', '🛒 Savatcham'],
+    ['📋 Buyurtmalarim', '👤 Profil'],
+    ['☎️ Yordam'],
+  ]).resize();
+}
+
+function inlineOpenShop(webAppUrl, path = '') {
+  return {
+    inline_keyboard: [[{ text: '🛍 Do\'konda ko\'rish', web_app: { url: webAppUrl + path } }]],
+  };
+}
+
+module.exports = { mainMenuKeyboard, inlineOpenShop };
 
