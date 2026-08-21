a1d8b00ab8c280ecfdda8b767af483cc0686f86d
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 11:29:21 +0500

    Update cart.js

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,1 +1,14 @@
 
+const express = require('express');
+const router = express.Router();
+const { requireTelegramAuth } = require('../middleware/telegramAuth');
+const { getCart, addToCart, updateCartItem, removeCartItem, clearCart } = require('../controllers/cartController');
+
+router.use(requireTelegramAuth);
+router.get('/', getCart);
+router.post('/', addToCart);
+router.put('/:itemId', updateCartItem);
+router.delete('/:itemId', removeCartItem);
+router.delete('/', clearCart);
+
+module.exports = router;
