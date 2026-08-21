4f9075a284dd697c867ed2a3747e0db004b2a13c
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-08-21 10:48:53 +0500

    Update Cart.js

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -1,51 +1,21 @@
 const mongoose = require('mongoose');
 
-const cartSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const CartItemSchema = new mongoose.Schema(
+  {
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true, min: 1, default: 1 },
+    size: { type: String, default: '' },
+    color: { type: String, default: '' },
   },
-  items: [{
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-      required: true
-    },
-    quantity: {
-      type: Number,
-      default: 1,
-      min: 1
-    },
-    size: String,
-    color: String,
-    price: Number,
-    addedAt: {
-      type: Date,
-      default: Date.now
-    }
-  }],
-  totalPrice: {
-    type: Number,
-    default: 0
-  },
-  totalItems: {
-    type: Number,
-    default: 0
-  },
-  couponCode: String,
-  discount: {
-    type: Number,
-    default: 0
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
+  { _id: true }
+);
 
-module.exports = mongoose.model('Cart', cartSchema);
+const CartSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true, index: true },
+    items: [CartItemSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Cart', CartSchema);
